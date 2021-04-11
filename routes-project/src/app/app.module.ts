b3ee4555e7113daa4f283da9ff10bc16a01879b1
Thiago Sousa <thiagosvin@hotmail.com>
2021-04-11 14:55:04 -0300

    Criando componentes 'courses', 'home' e 'login'

--- routes-project/src/app/app.module.ts
+++ routes-project/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
