c7b80aadccb8debbcd7b42b09f31180dbb8b447b
Thiago Sousa <thiagosvin@hotmail.com>
2021-04-12 09:17:26 -0300

    Rotas: RouterLink: definindo rotas no template - incompleto

--- routes-project/src/app/app.routing.ts
+++ routes-project/src/app/app.routing.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./login/login.component";
 
 const APP_ROUTES: Routes = [
-    {path:'cursos', component: CoursesComponent},
+    {path:'courses', component: CoursesComponent},
     {path: 'login', component: LoginComponent},
     { path: '', component: HomeComponent }
 ];
